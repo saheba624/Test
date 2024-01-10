6d72edb9d378040be12970e88613edee9a145b51
saheba624 <saheba624@gmail.com>
2024-01-10 10:36:12 +0530

    https://github.com/saheba624/Test.git

---
+++ JSP/ErrorOccurdCtl.java
@@ -0,0 +1,17 @@
+package com.ctl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns="/error")
+public class ErrorOccurdCtl extends  HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	throw new ServletException();
+	}
+}
